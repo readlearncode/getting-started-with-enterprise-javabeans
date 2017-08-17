2aa1aa61655a52f455537b902b8d4c860247730f
Alex Theedom <alex.theedom@gmail.com>
2017-08-17 17:53:01 +0100

    Add Singleton that populates the database with books

--- src/main/java/com/readlearncode/model/Book.java
+++ src/main/java/com/readlearncode/model/Book.java
@@ -10,154 +10,165 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Book implements Serializable {
 
-	public static final String FIND_ALL = "Book.findAll";
-	private static final float VAT_RATE = 5.5f;
+    public static final String FIND_ALL = "Book.findAll";
+    private static final float VAT_RATE = 5.5f;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id", updatable = false, nullable = false)
-	private Long id;
-	@Version
-	@Column(name = "version")
-	private int version;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+    @Version
+    @Column(name = "version")
+    private int version;
 
-	@Column(length = 17)
-	private String isbn;
+    @Column(length = 17)
+    private String isbn;
 
-	@Column(length = 50)
-	@NotNull
-	private String name;
+    @Column(length = 50)
+    @NotNull
+    private String name;
 
-	@Column(length = 300)
-	private String description;
+    @Column(length = 300)
+    private String description;
 
-	@Column(length = 255)
-	private String pictureURL;
+    @Column(length = 255)
+    private String pictureURL;
 
-	@Column(length = 13)
-	private Float price;
+    @Column(length = 13)
+    private Float price;
 
-	@Transient
-	private Float vat;
+    @Transient
+    private Float vat;
 
-	@Column
-	private String author;
+    @Column
+    private String author;
 
-	public Long getId() {
-		return this.id;
-	}
+    public Book() {}
 
-	public void setId(final Long id) {
-		this.id = id;
-	}
+    public Book(String isbn, String name, String description, String pictureURL, Float price, String author) {
+        this.isbn = isbn;
+        this.name = name;
+        this.description = description;
+        this.pictureURL = pictureURL;
+        this.price = price;
+        this.author = author;
+    }
 
-	public int getVersion() {
-		return this.version;
-	}
+    public Long getId() {
+        return this.id;
+    }
 
-	public void setVersion(final int version) {
-		this.version = version;
-	}
+    public void setId(final Long id) {
+        this.id = id;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof Book)) {
-			return false;
-		}
-		Book other = (Book) obj;
-		if (id != null) {
-			if (!id.equals(other.id)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public int getVersion() {
+        return this.version;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    public void setVersion(final int version) {
+        this.version = version;
+    }
 
-	public String getIsbn() {
-		return isbn;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Book)) {
+            return false;
+        }
+        Book other = (Book) obj;
+        if (id != null) {
+            if (!id.equals(other.id)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getIsbn() {
+        return isbn;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getPictureURL() {
-		return pictureURL;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setPictureURL(String pictureURL) {
-		this.pictureURL = pictureURL;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public Float getPrice() {
-		return price;
-	}
+    public String getPictureURL() {
+        return pictureURL;
+    }
 
-	public void setPrice(Float price) {
-		this.price = price;
-		if (price != null) {
-			vat = price * VAT_RATE / 100;
-		}
+    public void setPictureURL(String pictureURL) {
+        this.pictureURL = pictureURL;
+    }
 
-	}
+    public Float getPrice() {
+        return price;
+    }
 
-	public String getAuthor() {
-		return author;
-	}
+    public void setPrice(Float price) {
+        this.price = price;
+        if (price != null) {
+            vat = price * VAT_RATE / 100;
+        }
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
+    }
 
-	public Float getVat() {
-		return vat;
-	}
+    public String getAuthor() {
+        return author;
+    }
 
-	@Override
-	public String toString() {
-		String result = getClass().getSimpleName() + " ";
-		if (id != null)
-			result += "id: " + id;
-		result += ", version: " + version;
-		if (isbn != null && !isbn.trim().isEmpty())
-			result += ", isbn: " + isbn;
-		if (name != null && !name.trim().isEmpty())
-			result += ", name: " + name;
-		if (description != null && !description.trim().isEmpty())
-			result += ", description: " + description;
-		if (pictureURL != null && !pictureURL.trim().isEmpty())
-			result += ", pictureURL: " + pictureURL;
-		if (price != null)
-			result += ", price: " + price;
-		if (author != null && !author.trim().isEmpty())
-			result += ", author: " + author;
-		return result;
-	}
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Float getVat() {
+        return vat;
+    }
+
+    @Override
+    public String toString() {
+        String result = getClass().getSimpleName() + " ";
+        if (id != null)
+            result += "id: " + id;
+        result += ", version: " + version;
+        if (isbn != null && !isbn.trim().isEmpty())
+            result += ", isbn: " + isbn;
+        if (name != null && !name.trim().isEmpty())
+            result += ", name: " + name;
+        if (description != null && !description.trim().isEmpty())
+            result += ", description: " + description;
+        if (pictureURL != null && !pictureURL.trim().isEmpty())
+            result += ", pictureURL: " + pictureURL;
+        if (price != null)
+            result += ", price: " + price;
+        if (author != null && !author.trim().isEmpty())
+            result += ", author: " + author;
+        return result;
+    }
 }
---
+++ src/main/java/com/readlearncode/util/DatabaseInitialiser.java
@@ -0,0 +1,45 @@
+package com.readlearncode.util;
+
+import com.readlearncode.generator.ISBNGenerator;
+import com.readlearncode.generator.qualifier.ISBN10;
+import com.readlearncode.model.Book;
+import com.readlearncode.service.BookService;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+@Startup
+@Singleton
+public class DatabaseInitialiser {
+
+    @EJB
+    private BookService bookService;
+
+    @Inject
+    @ISBN10
+    ISBNGenerator isbnGenerator;
+
+    @PostConstruct
+    public void populateDatabase(){
+
+        Book book1 = new Book(isbnGenerator.generateISBN(), "Java EE by Example", "A book about Java EE", "cover/java-ee.jpg", 35.00f, "Alex Theedom");
+        bookService.save(book1);
+
+        Book book2 = new Book(isbnGenerator.generateISBN(), "JAX-RS and RESTful APIs", "A book about JAX-RS", "cover/jaz-rs.jpg", 15.00f, "Alex Theedom");
+        bookService.save(book2);
+
+    }
+
+
+
+
+}
